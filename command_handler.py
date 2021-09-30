f6f7f4a09e86520c537a227ae2d2b2841eff34ab
Zain Hussaini <zihussaini@honeybeerobotics.com>
2021-09-30 17:20:36 -0400

    version 2

---
+++ command_handler.py
@@ -0,0 +1,114 @@
+from workout_logger import WorkoutLogger
+from tabulate import tabulate
+
+DATA_FILE = "workouts.csv"
+
+
+def pretty_delta(diff):
+    # based on https://stackoverflow.com/a/5164027
+    s = diff.seconds
+    if diff.days > 7 or diff.days < 0:
+        return d.strftime('%d %b %y')
+    elif diff.days == 1:
+        return '1 day ago'
+    elif diff.days > 1:
+        return '{} days ago'.format(diff.days)
+    elif s <= 1:
+        return 'just now'
+    elif s < 60:
+        return '{} seconds ago'.format(s)
+    elif s < 120:
+        return '1 minute ago'
+    elif s < 3600:
+        return '{} minutes ago'.format(s//60)
+    elif s < 7200:
+        return '1 hour ago'
+    else:
+        return '{} hours ago'.format(s//3600)
+
+
+class CommandHandler:
+    def handle_command(message):
+        # remove surrounding white space
+        text = message.content.strip()
+
+        function = CommandHandler._find_command(text)
+        if function is None:
+            return None
+        else:
+            return function(message)
+
+    def _find_command(text):
+        if text.startswith("!"):
+            command = text.split(" ")[0][1:]
+            if command in COMMAND_MAP:
+                if len(text.split(" ")) != 1:
+                    return f"Command {command} does not take arguments"
+                return COMMAND_MAP[command]
+        return None
+
+    def _point(message):
+        name = str(message.author)
+        with WorkoutLogger(DATA_FILE) as wl:
+            time_diff, points = wl.add_workout(name)
+        if time_diff is None:
+            response = "You now have 1 point. Congrats on the first workout!"
+        else:
+            response = f"You now have {points} points. The workout before this was {pretty_delta(time_diff)}."
+        return response
+
+    def _loser(message):
+        name = str(message.author)
+        with WorkoutLogger(DATA_FILE) as wl:
+            removed_test = wl.remove_last_workout(name)
+        if removed_test:
+            points = get_points
+            if points == 0:
+                response = "You don't have any more points."
+            elif points == 1:
+                response = "You have 1 point left."
+            else:
+                response = f"You have {points} points left."
+        else:
+            response = "You don't have any points to remove!"
+        return response
+
+    def _scoreboard(message):
+        with WorkoutLogger(DATA_FILE) as wl:
+            sorted_names, name_to_points = wl.get_leaderboard()
+        if not sorted_names:
+            return "Scoreboard is empty!"
+        table = []
+        headers = ["Rank", "Name", "Points"]
+        for i, name in enumerate(sorted_names):
+            table.append([i+1, name, name_to_points[name]])
+        return "```\n" + tabulate(table, headers) + "\n```"
+
+    def _resetscoreboard(message):
+        with WorkoutLogger(DATA_FILE) as wl:
+            wl.reset_leaderboard()
+        response = "Scoreboard has been reset!"
+        return response
+
+    def _help(message):
+        help_items = [
+            "!point - add a workout",
+            "!loser - remove the last workout",
+            "!scoreboard - show the rankings",
+            "!resetscoreboard - remove all workouts",
+            "!help - show this help text"
+        ]
+        return "\n".join(help_items)
+
+
+COMMAND_MAP = {
+    "point": CommandHandler._point,
+    "loser": CommandHandler._loser,
+    "scoreboard": CommandHandler._scoreboard,
+    "resetscoreboard": CommandHandler._resetscoreboard,
+    "help": CommandHandler._help
+}
+
+
+if __name__ == "__main__":
+    print(CommandHandler._scoreboard(""))
--- main.py
+++ main.py
@@ -1,158 +1,38 @@
-import discord
-import redis
 import os
-import json
-from datetime import datetime
+import discord
+from dotenv import load_dotenv
+from command_handler import CommandHandler
 
-redis_server = redis.Redis() # Create access to Redis
 
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client()
 
-AUTH_TOKEN = str(redis_server.get('AUTH_TOKEN').decode('utf-8'))
-
-# keep track of whoever requested a reset
-# TODO: this will get cleared whenever this program stops running,
-# which might be ok or might not, I don't know...
-reset_requested = set()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    """Handle all startup tasks"""
+
+    if len(client.guilds) == 0:
+        print(f"{client.user} is not connected to any guilds")
+    else:
+        print(f"{client.user} is connected to the following guild{'s' if len(client.guilds) != 1 else ''}:")
+        for guild in client.guilds:
+            print(f"  {guild.name} (id: {guild.id})")
+
 
 @client.event
 async def on_message(message):
+    """Catch all for messages"""
 
-    # user = message.member.user
-
-    author=message.author
-    guild=message.guild
-
-    if author == client.user:
+    # don't react to your own messages
+    if message.author == client.user:
         return
 
-    ## create file if doesn't exist, and get discord channel data
-    fname = str(guild.id) + '.json'
-    data = {}
-    try:
-      with open(fname) as json_file:
-        data = json.load(json_file)
-    except OSError:
-      print ("Adding Discord Channel" + guild.name + ":" + fname)
-      with open(fname, 'w') as f:
-        json.dump(data, f)
-
-    if message.content == '!point':
-      aid = str(author.id)
-      if aid not in data.keys():
-        data[aid] = {'points': 0, 'last_workout': datetime.now()}
-
-      data[aid]['points'] += 1
-      date[aid]['last_workout'] = datetime.now()
-
-      with open(fname, 'w') as f:
-        json.dump(data, f)
-      await message.channel.send(points_print(author.id, data[aid]['points']))
-
-    elif message.content == '!loser':
-      aid = str(author.id)
-      if aid in data.keys():
-        if (data[aid]['points'] > 0):
-          data[aid]['points'] -= 1
-          # get dropped to bottom of people with tied points
-          data[aid]['last_workout'] = datetime.max
-
-          with open(fname, 'w') as f:
-            json.dump(data, f)
-          await message.channel.send(points_print(author.id, data[aid]['points']))
-
-        else:
-          await message.channel.send('<@!{}> has no points!'.format(author.id))
-
-      else:
-        await message.channel.send('<@!{}> has never even made a point!'.format(author.id))
-
-    elif message.content == '!reset':
-      aid = str(author.id)
-      if aid in data.keys():
-        data[aid]['points'] = 0
-        # get dropped to bottom of list
-        data[aid]['last_workout'] = datetime.max
-
-        with open(fname, 'w') as f:
-          json.dump(data, f)
-        await message.channel.send('<@!{}> has reset their score to 0'.format(author.id))
-
-    elif message.content == '!quitter':
-      aid = str(author.id)
-      if aid in data.keys():
-        data.pop(aid)
-
-        with open(fname, 'w') as f:
-          json.dump(data, f)
-        await message.channel.send('<@!{}> has quit the competition, how pathetic'.format(author.id))
-
-    elif message.content == '!scoreboard':
-      # sort aids by:
-      # 1. points (highest first, so use -points)
-      # 2. last_workout (lowest first)
-      aids = data.keys()
-
-      if aids:
-        sortable_list = [(-data[aid]['points'], data[aid]['last_workout'], aid) for aid in aids]
-        sorted_list = sorted(sortable_list)
-
-        string = ''
-        for _, _, aid in sorted_list:
-          member = await guild.fetch_member(int(aid))
-          string += '{}: {}'.format(member, data[aid]['points'])
-          string += '\n'
-
-        await message.channel.send(string)
-      else:
-        string = 'scoreboard is empty'
-        await message.channel.send(string)
-
-    elif message.content == '!resetscoreboard':
-      aid = str(author.id)
-
-      if not data.keys():
-          string = 'scoreboard is already empty!'
-          await message.channel.send(string)
-
-      # use system of super majority to reset
-      # ex. with 6 people 4 people need to vote to reset
-      # ex. with 5 people 3 people need to vote to reset
-      reset_requirement = int(len(data.keys())/2)+1
-      if aid in reset_requested:
-        string = f'You already requested a reset. Currently {len(reset_requested)} of {reset_requirement} needed have requested the reset'
-        await message.channel.send(string)
-      else:
-        reset_requested.add(aid)
-        if len(reset_requested) >= reset_requested:
-          data = {}
-          reset_requested = set()
-          with open(fname, 'w') as f:
-            json.dump(data, f)
-          await message.channel.send('All scores reset!')
-        else:
-          string = f'Your vote to reset has been counted. Currently {len(reset_requested)} of {reset_requirement} needed have requested the reset'
-          await message.channel.send(string)
-
-    elif message.content == '!help':
-      string = ''
-      string += '!point: give yourself a point\n'
-      string += '!loser: remove a point of yours\n'
-      string += '!reset: set your points to 0\n'
-      string += '!quitter: remove yourself from the competition\n'
-      string += '!scoreboard: displays the scores of all users\n'
-      string += '!resetscoreboard: vote to set all user points to 0'
-      await message.channel.send(string)
-
-def points_print(aid, points):
-      if points == 1:
-        return '<@!{}> now has {} point'.format(aid, points)
-      else:
-        return '<@!{}> now has {} points'.format(aid, points)
+    response = CommandHandler.handle_command(message)
+    if response is not None:
+        await message.channel.send(response)
 
 
-client.run(AUTH_TOKEN)
+client.run(TOKEN)
